1bb33e3b91b7b7b68656f0a9658547bbecda7bb0
thomas-storm <dr.storm@web.de>
2023-12-28 16:04:06 +0100

    Commit Final

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix contains two functions: makeCacheMatrix() and cacheSolve()
+## makeCacheMatrix() creates an R object that stores a matrix and its inverse
+## cacheResolve requires a matrix as argument from makeCacheMatrix() and calculates the inverse of the matrix
+## Helpful resource: https://github.com/lgreski/datasciencectacontent/blob/master/markdown/rprog-breakingDownMakeVector.md
+## Explains the vector example for this exercise very well
 
-## Write a short comment describing this function
+## Create R object to store a matrix and provide environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Retrieve cached matrix and inverse, or calculate inverse if not cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+        
 }
-## Test that Git works from different laptop
